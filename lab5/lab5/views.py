6e0b374eda3332cfdaa4f29f92e593982df7ca7c
Vladimir Gololobov <39838109+blakexdd@users.noreply.github.com>
2019-11-19 18:37:39 +0300

    Lab5 general picture
    Created django project with start page and "hello" page

---
+++ lab5/lab5/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return HttpResponse("Hello, world!")
+
+def indexRender(request):
+    return render(request, 'index.html', {})
